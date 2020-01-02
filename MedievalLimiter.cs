369444589b2a69220c0f8a0096614ba53a224219
kevinatoranator <kevguy.vet@gmail.com>
2020-01-02 16:46:27 -0500

    Add files via upload

---
+++ MedievalLimiter.cs
@@ -0,0 +1,23 @@
+/*
+ * User: Kevin Rinald
+ * 
+ */
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Rimworld;
+using Rimworld.BaseGen;
+using Verse;
+
+namespace HighFantasy
+{
+	/// <summary>
+	/// Description of MyClassName.
+	/// </summary>
+	public static class MedievalLimiter
+	{
+		public MyClassName()
+		{
+		}
+	}
+}
